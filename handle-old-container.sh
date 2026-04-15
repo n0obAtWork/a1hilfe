0fc3e0519a68706ee2e2140ff4b8f9e39122359e
Jeffrey Fernandez <jeffrey.fernandez@amic.de>
2026-04-15 07:27:33 +0200

    Pipeline - container check

--- handle-old-container.sh
+++ handle-old-container.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Greeting my friend"
+container_name="spike-a1hilfe-mdbook"
+
+# Check if the container exists
+if docker inspect "$container_name" > /dev/null 2>&1; then
+    echo "The container $container_name exists."
+    
+    # Check if the container is running
+    if $(docker inspect -f '{{.State.Status}}' "$container_name" | grep -q "running"); then
+        echo "The container $container_name is running - stopping it now"
+        docker stop "$container_name"
+    fi
+
+    echo "Removing container $container_name"
+    docker rm "$container_name"
+fi
